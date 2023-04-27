047e874dc3fcf69c17236f062870cf9a70c04e0b
Oliver Juggins <oliver.juggins@gmail.com>
2023-04-27 21:22:08 +0200

    iteration 8

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,52 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      if (printTimeCallback) {
+        printTimeCallback();
+      }
+      this.currentTime++;
+    }, 1000);
   }
 
   getMinutes() {
-    // ... your code goes here
+    const minutesPassed = this.currentTime / 60;
+    return Math.floor(minutesPassed);
   }
 
   getSeconds() {
-    // ... your code goes here
+    const seconds = this.currentTime % 60;
+    return seconds;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    const valueAsString = value.toString();
+    if (valueAsString.length === 1) {
+      return `${0+valueAsString}`
+    }
+    else {
+      return valueAsString;
+    }
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    const splitMinutes = this.getMinutes();
+    const splitSeconds = this.getSeconds();
+    const splitMinutesString = this.computeTwoDigitNumber(splitMinutes);
+    const splitMinutesSeconds = this.computeTwoDigitNumber(splitSeconds);
+    const splitString = `${splitMinutesString}:${splitMinutesSeconds}`;
+    return splitString;
   }
 }
